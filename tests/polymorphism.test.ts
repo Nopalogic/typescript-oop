6d4646ee87b367430a7e64ad0f552e90ab152e91
Nopalogic <naufaladhi02@gmail.com>
2023-09-13 01:45:17 +0700

    Method Polymorphism

--- tests/polymorphism.test.ts
+++ tests/polymorphism.test.ts
@@ -6,6 +6,10 @@ describe('Polymorphism', () => {
 	class Manager extends Employee {}
 	class VicePresident extends Manager {}
 
+	function sayHello(employee: Employee): void {
+		console.info(`Hello ${employee.name}`);
+	}
+
 	it('should support polymorphism', () => {
 		let employee: Employee = new Employee('Ucup');
 		console.info(employee);
@@ -15,5 +19,11 @@ describe('Polymorphism', () => {
 
 		employee = new VicePresident('Asep');
 		console.info(employee);
+	});
+
+	it('should support method parameter polmorphism', () => {
+		sayHello(new Employee('Ucup'));
+		sayHello(new Manager('Udin'));
+		sayHello(new VicePresident('Asep'));
 	});
 });
